2a93be7343d182692bfc0bd6262c4ed7e2e05398
andre d. orotea <andre.orotea@psi.com>
2023-08-26 07:57:21 +0800

    second commit

---
+++ database/migrations/2023_08_25_232547_create_cars_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('mmpcmodelcode');
+            $table->year('mmpcmodelyear');
+            $table->string('mmpcoptioncode');
+            $table->string('extcolorcode');
+            $table->string('modeldescription');
+            $table->string('exteriorcolor');
+            $table->string('csno')->nullable();
+            $table->date('bilingdate');
+            $table->string('vehicleidno')->unique()->nullable();
+            $table->string('engineno')->nullable();
+            $table->imteger('productioncbunumber')->nullable();
+            $table->imteger('bilingdocuments')->nullable();
+            $table->string('vehiclestockyard')->nullable();
+            $table->timestamp('car_recieve_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
