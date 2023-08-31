849c463f5b84584ee6a0585b58fb9f5ff153084a
ashintaru <109757954+ashintaru@users.noreply.github.com>
2023-08-31 16:53:36 +0800

    last comit this 31 / 8 / 2023

---
+++ resources/views/profile/partials/dens-form.blade.php
@@ -0,0 +1,64 @@
+<section>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <form method="POST" action="{{URL('loose-item/'.$carid)}}">
+            @csrf
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead>
+                    <tr>
+                        <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                            SET OF TOOL'S
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b border-gray-200 dark:border-gray-700">                    
+                        <td class="px-6 py-4">
+                            <div class="flex items-center mb-4">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="border-b border-gray-200 dark:border-gray-700">                    
+                        <td class="px-6 py-4">
+                            <div class="flex items-center mb-4">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="border-b border-gray-200 dark:border-gray-700">                    
+                        <td class="px-6 py-4">
+                            <div class="flex items-center mb-4">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="border-b border-gray-200 dark:border-gray-700">                    
+                        <td class="px-6 py-4">
+                            <div class="flex items-center mb-4">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="border-b border-gray-200 dark:border-gray-700">                    
+                        <td class="px-6 py-4">
+                            <div class="flex items-center mb-4">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-6 py-4">
+                            <x-primary-button>Submit</x-primary-button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
+
+</section>
--- resources/views/viewcar.blade.php
+++ resources/views/viewcar.blade.php
@@ -36,6 +36,26 @@
             </div>
         </div>
         @endif
+
+        <div class="py-4">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    @include('profile.partials.tool-form',['carid'=>$car->vehicleidno])
+                </div>
+                <x-alert-error></x-alert-error>
+                <x-alert-success></x-alert-success>
+            </div>
+        </div>
+        
+        <div class="py-4">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    @include('profile.partials.dens-form',['carid'=>$car->vehicleidno])
+                </div>
+                <x-alert-error></x-alert-error>
+                <x-alert-success></x-alert-success>
+            </div>
+        </div>
         @if ($car->havebeenstored==0)
             <div class="py-4">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
