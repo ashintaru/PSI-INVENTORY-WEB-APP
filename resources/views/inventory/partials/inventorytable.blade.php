31a468fe8fb117e3728fff378fae54476d9a2354
ashintaru <109757954+ashintaru@users.noreply.github.com>
2023-09-30 12:47:56 +0800

    Update inventorytable.blade.php

--- resources/views/inventory/partials/inventorytable.blade.php
+++ resources/views/inventory/partials/inventorytable.blade.php
@@ -3,8 +3,11 @@
        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                <th scope="col" class="px-3 py-3">
-                   #
+                   Inv ID
                </th>
+               <th scope="col" class="px-3 py-3">
+                    Data ID
+                </th>
                <th scope="col" class="px-6 py-3">
                     <p class="flex items-center text-sm text-gray-500 dark:text-gray-400">Passed
                          <button data-popover-target="popover-description" data-popover-placement="right" type="button"><svg class="w-4 h-4 ml-2 text-gray-400 hover:text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg><span class="sr-only">Show information</span></button></p>
@@ -54,6 +57,9 @@
                                 {{$d->id}}
                             </th>
                             <th scope="row" class="px-3 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{$d->car->id}}
+                            </th>
+                            <th scope="row" class="px-3 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 @if ($d->invstatus == 1)
                                     <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">R.F.I</span>
                                 @else
@@ -75,6 +81,15 @@
                             <td class="px-6 py-4 flex ">
                                 {{$d->car->billingdate}}
                             </td>
+                            <td class="px-6 py-4 flex ">
+                                <a href="{{URL('/view'."/".$d->car->id)}}"
+                                    data-tooltip-target="config-button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                                        <path fill-rule="evenodd" d="M14.5 10a4.5 4.5 0 004.284-5.882c-.105-.324-.51-.391-.752-.15L15.34 6.66a.454.454 0 01-.493.11 3.01 3.01 0 01-1.618-1.616.455.455 0 01.11-.494l2.694-2.692c.24-.241.174-.647-.15-.752a4.5 4.5 0 00-5.873 4.575c.055.873-.128 1.808-.8 2.368l-7.23 6.024a2.724 2.724 0 103.837 3.837l6.024-7.23c.56-.672 1.495-.855 2.368-.8.096.007.193.01.291.01zM5 16a1 1 0 11-2 0 1 1 0 012 0z" clip-rule="evenodd" />
+                                        <path d="M14.5 11.5c.173 0 .345-.007.514-.022l3.754 3.754a2.5 2.5 0 01-3.536 3.536l-4.41-4.41 2.172-2.607c.052-.063.147-.138.342-.196.202-.06.469-.087.777-.067.128.008.257.012.387.012zM6 4.586l2.33 2.33a.452.452 0 01-.08.09L6.8 8.214 4.586 6H3.309a.5.5 0 01-.447-.276l-1.7-3.402a.5.5 0 01.093-.577l.49-.49a.5.5 0 01.577-.094l3.402 1.7A.5.5 0 016 3.31v1.277z" />
+                                      </svg>
+                                </a>
+                            </td>
                         </tr>
                     @endif
                 @endforeach
@@ -82,6 +97,6 @@
                 table are empty.......
             @endif
        </tbody>
-   </table>
+    </table>
  </section>
 
