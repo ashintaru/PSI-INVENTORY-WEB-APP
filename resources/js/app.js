f28311c7bbab86b888ee625767f893bb71ff8840
ashintaru <109757954+ashintaru@users.noreply.github.com>
2023-11-10 00:12:43 +0800

    qwe

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,20 @@
 import './bootstrap';
+
+
+document.addEventListener('livewire:navigated', () => {
+    // Mutate the HTML before the page is navigated away...
+    initFlowbite();
+})
+
 import 'flowbite';
 import 'flowbite-datepicker';
 
+
+
 import Alpine from 'alpinejs';
+import { initFlowbite } from 'flowbite';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -205,12 +205,12 @@
                     </li>
                   </ul>
                   <div class="py-1">
-                    <a href="{{URL('recieve')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Receiving Unit / Inspection</a>
+                    <a wire:navigate href="{{URL('recieve')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Receiving Unit / Inspection</a>
                   </div>
               </div>
             </li>
           <li>
-            <a href="{{URL('inventory')}}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+            <a wire:navigate href="{{URL('inventory')}}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                     <path d="M2 3a1 1 0 00-1 1v1a1 1 0 001 1h16a1 1 0 001-1V4a1 1 0 00-1-1H2z" />
                     <path fill-rule="evenodd" d="M2 7.5h16l-.811 7.71a2 2 0 01-1.99 1.79H4.802a2 2 0 01-1.99-1.79L2 7.5zM7 11a1 1 0 011-1h4a1 1 0 110 2H8a1 1 0 01-1-1z" clip-rule="evenodd" />
