1977b266d609bce67a3f11a1050a799ee3f251a2
ashintaru <109757954+ashintaru@users.noreply.github.com>
2023-12-02 15:23:53 +0800

    tuloy mamaya

--- app/Livewire/Site.php
+++ app/Livewire/Site.php
@@ -9,7 +9,39 @@ use Livewire\Attributes\On;
 
 class Site extends Component
 {
+    public $siteid = '';
     public $sitename = '';
+    public $siteNameEdit = '';
+
+    public $isEditingSite = true;
+
+
+
+    public function editSite($siteId){
+        $this->siteid = $siteId;
+        $this->isEditingSite = true;
+    }
+
+    public function updateSite(){
+
+        $validate = Validator::make(
+            ['siteNameEdit' => $this->siteNameEdit, 'siteId'=>$this->siteid ],
+            // Validation rules to apply...
+            ['siteNameEdit' => 'required|min:3' , 'siteId' => 'required' ],
+            // Custom validation messages...
+            ['required' => 'The :attribute field is required'],
+        )->validate();
+        if(modelSite::where('siteName',$this->siteNameEdit)->exists()){
+            session()->flash('warning','the sitename are alreasy exsist');
+        }else{
+            $site = modelSite::where('id',$this->siteid)->first();
+            $site->siteName = strtoupper($this->siteNameEdit);
+            $site->save();
+            session()->flash('success','the site have been update properly');
+            $this->reset(['siteid','isEditingSite','sitename']);
+        }
+
+    }
 
     public function saveSite(){
         $validate = Validator::make(
